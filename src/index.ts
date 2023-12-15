90349635ae415f5b9aa40864b83c7d4183ab34df
mikheykinnv <mikheykinnv@txix.ru>
2023-12-15 13:53:27 +0530

    MSP-2530 add InputNumber

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import FormCheckbox from './FormCheckbox/FormCheckbox';
 import FormInput from './FormInput/FormInput';
+import FormInputNumber from './FormInputNumber/FormInputNumber';
 import MyButton from './MyButton/MyButton';
 import MyInput from './MyInput/MyInput';
 import * as Layout from './Layout';
@@ -7,4 +8,4 @@ import SearchInput from './SearchInput/SearchInput';
 import useAuth from './hooks/useAuth';
 import { getConfigProperty } from './utils/configUtils';
 
-export { MyButton, MyInput, FormCheckbox, FormInput, SearchInput, Layout, useAuth, getConfigProperty };
+export { MyButton, MyInput, FormCheckbox, FormInput, FormInputNumber, SearchInput, Layout, useAuth, getConfigProperty };
