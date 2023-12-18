306e700ea8cf0a05f43ef360f69219ac5bc231fc
veselovnk <ves.nikita1303@gmail.com>
2023-12-18 14:46:51 +0300

    MSP-2531 add HomePage component

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -2,3 +2,4 @@ export { default as Column } from './Column/Column';
 export { default as ContentColumn } from './ContentColumn/ContentColumn';
 export { default as NoData } from './NoData/NoData';
 export { default as EmptyData } from './EmptyData/EmptyData';
+export { default as HomePage } from './HomePage/HomePage';
