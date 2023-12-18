e0549140b2ff336427db2b1c4bcd8a6ce44d938f
veselovnk <ves.nikita1303@gmail.com>
2023-12-18 15:07:33 +0300

    MSP-2531 add ErrorPage component

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -3,3 +3,4 @@ export { default as ContentColumn } from './ContentColumn/ContentColumn';
 export { default as NoData } from './NoData/NoData';
 export { default as EmptyData } from './EmptyData/EmptyData';
 export { default as HomePage } from './HomePage/HomePage';
+export { default as ErrorPage } from './ErrorPage/ErrorPage';
