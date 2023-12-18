306bf9f830f2885e4dd0a026d62b0da8ef62116d
veselovnk <ves.nikita1303@gmail.com>
2023-12-18 15:22:42 +0300

    MSP-2531 add NotFoundPage component

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -4,3 +4,4 @@ export { default as NoData } from './NoData/NoData';
 export { default as EmptyData } from './EmptyData/EmptyData';
 export { default as HomePage } from './HomePage/HomePage';
 export { default as ErrorPage } from './ErrorPage/ErrorPage';
+export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
