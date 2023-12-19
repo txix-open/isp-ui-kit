e4f033230c5d78de8543fb5323037960724abf39
mikheykinnv <mikheykinnv@txix.ru>
2023-12-19 11:32:27 +0530

    MSP-2530 add InputPassword

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import FormCheckbox from './FormCheckbox/FormCheckbox';
 import FormInput from './FormInput/FormInput';
 import FormInputNumber from './FormInputNumber/FormInputNumber';
+import FormInputPassword from './FormInputPassword/FormInputPassword';
 import MyButton from './MyButton/MyButton';
 import MyInput from './MyInput/MyInput';
 import * as Layout from './Layout';
@@ -8,4 +9,4 @@ import SearchInput from './SearchInput/SearchInput';
 import useAuth from './hooks/useAuth';
 import { getConfigProperty } from './utils/configUtils';
 
-export { MyButton, MyInput, FormCheckbox, FormInput, FormInputNumber, SearchInput, Layout, useAuth, getConfigProperty };
+export { MyButton, MyInput, FormCheckbox, FormInput, FormInputNumber, FormInputPassword, SearchInput, Layout, useAuth, getConfigProperty };
