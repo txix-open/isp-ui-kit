a3d4e06dcd61a0ba615c296869392c5c49f311e8
mikheykinnv <mikheykinnv@txix.ru>
2023-12-20 15:03:27 +0530

    MSP-2530 add FormTextArea

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import FormInputNumber from './FormInputNumber/FormInputNumber';
 import FormInputPassword from './FormInputPassword/FormInputPassword';
 import FormRadioGroup from './FormRadioGroup/FormRadioGroup';
 import FormSelect from './FormSelect/FormSelect';
+import FormTextArea from './FormTextArea/FormTextArea';
 import MyButton from './MyButton/MyButton';
 import MyInput from './MyInput/MyInput';
 import * as Layout from './Layout';
@@ -11,4 +12,4 @@ import SearchInput from './SearchInput/SearchInput';
 import useAuth from './hooks/useAuth';
 import { getConfigProperty } from './utils/configUtils';
 
-export { MyButton, MyInput, FormCheckbox, FormInput, FormRadioGroup, FormInputNumber, FormInputPassword, FormSelect, SearchInput, Layout, useAuth, getConfigProperty };
+export { MyButton, MyInput, FormCheckbox, FormInput, FormRadioGroup, FormInputNumber, FormInputPassword, FormSelect, FormTextArea, SearchInput, Layout, useAuth, getConfigProperty };
---
+++ src/stories/TextArea.stories.tsx
@@ -0,0 +1,74 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useForm } from 'react-hook-form';
+import FormTextArea from '../FormTextArea/FormTextArea';
+
+const meta: Meta<typeof FormTextArea> = {
+  component: FormTextArea,
+  title: 'TextArea',
+  args: {
+    label: 'Название TextArea',
+    name: 'TextArea',
+    rules: { required: { value: true, message: 'Поле не может быть пустым' } },
+  },
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'Компонент TextArea, который поддерживает react-hook-form',
+      },
+    },
+  },
+  argTypes: {
+    control: {
+      control: false,
+      description: 'параметр, получаемый из react-hook-form',
+    },
+    name: {
+      control: false,
+      description: 'Путь до поля в структуре',
+    },
+    label: {
+      description: 'Подпись к TextArea',
+    },
+    rules: {
+      description: 'Правила валидации поля',
+    },
+    controlClassName: {
+      description: 'Имя класса для компонента формы',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FormTextArea>;
+const onSubmit = (data: unknown) => console.log(data);
+
+export const TextArea: Story = {
+  render: (args) => {
+    const methods = useForm();
+    const { control, handleSubmit } = methods;
+    args.control = control;
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormTextArea {...args} />
+      </form>
+    );
+  },
+};
+export const Validation: Story = {
+  render: (args) => {
+    const methods = useForm();
+    const { control, handleSubmit } = methods;
+    args.control = control;
+    control.setError('TextArea', {
+      type: 'required',
+      message: args.rules!.required!.message,
+    });
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormTextArea {...args} />
+      </form>
+    );
+  },
+};
