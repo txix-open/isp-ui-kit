c5c6183391429bdf1e78ed7532dc0fbfb2aef5f3
veselovnk <ves.nikita1303@gmail.com>
2024-06-18 15:54:26 +0300

    MSP-3466 add module declaration for ReactJsonView

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import * as Layout from './Layout';
 import SearchInput from './SearchInput/SearchInput';
 import useAuth from './hooks/useAuth';
 import { getConfigProperty } from './utils/configUtils';
-// @ts-ignore
 import ReactJsonView from './ReactJsonView/js';
 
 export {
