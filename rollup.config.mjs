3eb697eeb71c923477632912bc30396f990a01f2
veselovnk <ves.nikita1303@gmail.com>
2024-06-18 15:54:48 +0300

    MSP-3466 change webpack to rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,43 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import postcss from "rollup-plugin-postcss";
+
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import image from '@rollup/plugin-image';
+import {babel} from "@rollup/plugin-babel";
+
+export default [
+    {
+        input: "src/index.ts",
+
+        output: {
+            file: "dist/index.js",
+            format: "esm",
+        },
+        plugins: [
+            babel({
+                babelHelpers: "bundled",
+                exclude: 'node_modules/**'
+            }),
+            peerDepsExternal(),
+            resolve({
+                extensions: ['.js', '.jsx'],
+            }),
+            commonjs(),
+            typescript({tsconfig: "./tsconfig.json"}),
+            postcss({minimize: true}),
+            terser({
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true,
+                },
+                output: {
+                    comments: false,
+                },
+            }),
+            image()
+        ],
+    }
+];
