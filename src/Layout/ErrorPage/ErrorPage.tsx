1b1d733c35c8b6a6e9bf6016539f3b4855c8afe6
txix <txix@txix.ru>
2024-07-03 11:54:26 +0300

    2024-07-03 11:54:26: merge [main:1cfc7efed5b9a328718c6e4cc63e38d6df674a42];

--- src/Layout/ErrorPage/ErrorPage.tsx
+++ src/Layout/ErrorPage/ErrorPage.tsx
@@ -1,14 +1,16 @@
-import { Result } from 'antd';
+import { Layout, Result } from 'antd';
 import { FC } from 'react';
 import { ErrorPageProps } from './error-page';
 
 const ErrorPage: FC<ErrorPageProps> = ({ children = null }) => (
-  <Result
-    status="500"
-    title="500"
-    subTitle="Извините, что-то пошло не так."
-    extra={children}
-  />
+  <Layout className="error-page">
+    <Result
+      status="500"
+      title="500"
+      subTitle="Извините, что-то пошло не так."
+      extra={children}
+    />
+  </Layout>
 );
 
 export default ErrorPage;
