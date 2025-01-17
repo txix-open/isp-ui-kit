321753e71883341b86ef1b759abe9ab3edada838
foxxy <veselovnk@txix.ru>
2025-01-17 13:50:34 +0300

    MSP-4745 update project deps

--- src/Layout/Column/Column.tsx
+++ src/Layout/Column/Column.tsx
@@ -31,11 +31,11 @@ const Column = <T extends {}>({
 }: ColumnProps<T>) => {
   const isDisabled = !selectedItemId;
   const refs: Record<string, RefObject<HTMLDivElement>> = items.reduce(
-    (acc: Record<string, RefObject<HTMLDivElement>>, value) => {
-      acc[value.id] = createRef<HTMLDivElement>();
-      return acc;
-    },
-    {},
+      (acc: Record<string, RefObject<HTMLDivElement>>, value) => {
+        acc[value.id] = createRef<HTMLDivElement>();
+        return acc;
+      },
+      {},
   );
 
   useEffect(() => {
