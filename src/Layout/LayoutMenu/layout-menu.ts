fce6b11cf94ed0fe8f574f767a3a44f80bb0eed3
Anastasiia <anufrievaad@txix.ru>
2025-01-28 12:48:02 +0300

    MSP-4771 create LayoutMenu

---
+++ src/Layout/LayoutMenu/layout-menu.ts
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+
+export interface ConfigMenuItemType {
+  route?: string;
+  label: string;
+  key: string;
+  permissions: string | string[];
+  icon?: ReactNode;
+  children?: ConfigMenuItemType[];
+}
+
+export interface MenuItemType {
+  label: string;
+  key: string;
+  className?: string;
+  icon?: ReactNode;
+  children?: MenuItemType[];
+}
+
+export interface LayoutMenuPropsType {
+  onHideMenuItem: (value: string | string[]) => string;
+  menuConfig: ConfigMenuItemType[];
+  onClickItem: ({ key }: { key: string }) => void;
+  currentPath: string;
+}
