6e686cb22f7464e0bf32c3ccad07a238c5d574e5
mikheykinnv <mikheykinnv@txix.ru>
2025-03-04 15:49:48 +0800

    MSP_5021 add FormAutoComplete stories

---
+++ src/stories/formComponentsStories/AutoComplite.stories.tsx
@@ -0,0 +1,84 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useForm } from 'react-hook-form';
+import { FormAutoComplete } from '../../FormComponents';
+
+const meta: Meta<typeof FormAutoComplete> = {
+  component: FormAutoComplete,
+  title: 'FormComponents/FormAutoComplete',
+  args: {
+    label: 'Название AutoComplete',
+    name: 'AutoComplete',
+    rules: { required: { value: true, message: 'Поле не может быть пустым' } },
+    options: [{ value: 'http' }, { value: 'https' }],
+  },
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component:
+          'Компонент AutoComplete, который поддерживает react-hook-form',
+      },
+    },
+  },
+  argTypes: {
+    control: {
+      control: false,
+      description: 'параметр, получаемый из react-hook-form',
+    },
+    name: {
+      control: false,
+      description: 'Путь до поля в структуре',
+    },
+    label: {
+      description: 'Подпись к AutoComplete',
+    },
+    rules: {
+      description: 'Правила валидации',
+    },
+    controlClassName: {
+      description: 'Имя класса для компонента формы',
+    },
+    options: {
+      description:
+        'Ожидает массив объектов { value: "значение" } для дефлотных параметров',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FormAutoComplete>;
+
+const onSubmit = (data: unknown) => console.log(data);
+
+export const AutoComplete: Story = {
+  render: (args) => {
+    const methods = useForm();
+    const { control, handleSubmit } = methods;
+    args.control = control;
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormAutoComplete {...args} />
+      </form>
+    );
+  },
+};
+
+export const Validation: Story = {
+  render: (args) => {
+    const methods = useForm();
+    const { control, handleSubmit } = methods;
+    args.control = control;
+
+    control.setError('AutoComplete', {
+      type: 'required',
+      message: args.rules!.required!.message,
+    });
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormAutoComplete {...args} />
+      </form>
+    );
+  },
+};
