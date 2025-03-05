f9bc39dceb0a36ceb899194c1b7681e0f51eaa1c
txix <txix@txix.ru>
2025-03-05 12:14:36 +0300

    2025-03-05 12:14:36: merge [main:300292699573d464f15df62e47d6955448b215c8];

--- src/cfg/dayjs-config.ts
+++ src/cfg/dayjs-config.ts
@@ -1,8 +1,20 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import localeData from 'dayjs/plugin/localeData';
+import weekday from 'dayjs/plugin/weekday';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+import weekYear from 'dayjs/plugin/weekYear';
 import 'dayjs/locale/ru';
 
+dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.extend(weekOfYear);
+dayjs.extend(weekYear);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.locale('ru');
