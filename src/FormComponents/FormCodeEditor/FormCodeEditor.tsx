adcb22960ca0f348e44cea8211752e9a94a29361
Nikita Veselov <veselovnk@txix.ru>
2025-12-12 13:56:32 +0300

    MSP-6785 update FormCodeEditor initialization logic

--- src/FormComponents/FormCodeEditor/FormCodeEditor.tsx
+++ src/FormComponents/FormCodeEditor/FormCodeEditor.tsx
@@ -1,16 +1,8 @@
 import { Controller, FieldValues } from 'react-hook-form';
 import Editor, { loader } from '@monaco-editor/react';
-import * as monaco from 'monaco-editor';
+// import * as monaco from 'monaco-editor';
 import { FormCodeEditorProps } from './form-code-editor.type';
-
-loader.config({
-  ...monaco,
-  'vs/nls': {
-    availableLanguages: {
-      '*': 'ru',
-    },
-  },
-});
+import { useEffect, useState } from 'react';
 
 const formatJSON = (val: string) => {
   try {
@@ -21,6 +13,20 @@ const formatJSON = (val: string) => {
   }
 };
 
+let monacoLoaderPromise: Promise<void> | null = null;
+
+const initMonaco = () => {
+  if (!monacoLoaderPromise) {
+    monacoLoaderPromise = import('monaco-editor').then((monaco) => {
+      loader.config({
+        monaco,
+        'vs/nls': { availableLanguages: { '*': 'ru' } },
+      });
+    });
+  }
+  return monacoLoaderPromise;
+};
+
 export default <T extends FieldValues>({
   control,
   name,
@@ -28,30 +34,41 @@ export default <T extends FieldValues>({
   disable = false,
   language = 'javascript',
   ...rest
-}: FormCodeEditorProps<T>) => (
-  <Controller
-    control={control}
-    name={name}
-    render={({ field: { onChange, value } }) => (
-      <Editor
-        options={{ readOnly: disable }}
-        height={height}
-        value={value}
-        language={language}
-        onChange={(value) => onChange(value)}
-        onMount={(editor) => {
-          editor.onDidBlurEditorWidget(() => {
-            const currentValue = editor.getValue();
-            const formattedValue = formatJSON(currentValue);
+}: FormCodeEditorProps<T>) => {
+  const [isReady, setIsReady] = useState(false);
 
-            if (formattedValue !== currentValue) {
-              onChange(formattedValue);
-              editor.setValue(formattedValue);
-            }
-          });
-        }}
-        {...rest}
-      />
-    )}
-  />
-);
+  useEffect(() => {
+    initMonaco().then(() => setIsReady(true));
+  }, []);
+
+  if (!isReady) {
+    return <div>Loading editor...</div>; // Или null, или спиннер
+  }
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, value } }) => (
+        <Editor
+          options={{ readOnly: disable }}
+          height={height}
+          value={value}
+          language={language}
+          onChange={(value) => onChange(value)}
+          onMount={(editor) => {
+            editor.onDidBlurEditorWidget(() => {
+              const currentValue = editor.getValue();
+              const formattedValue = formatJSON(currentValue);
+
+              if (formattedValue !== currentValue) {
+                onChange(formattedValue);
+                editor.setValue(formattedValue);
+              }
+            });
+          }}
+          {...rest}
+        />
+      )}
+    />
+  );
+};
